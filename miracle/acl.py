949756dea24d22201673a074360619887bda461a
Mark Vartanyan <kolypto@gmail.com>
2013-12-30 16:38:12 +0200

    add_permission(), del_permission(), list_permissions()

--- miracle/acl.py
+++ miracle/acl.py
@@ -27,7 +27,7 @@ class Acl(object):
         return self
 
     def add_resource(self, resource):
-        """ Define resources.
+        """ Define a resource.
 
             :param resource: Resource to define.
                 For now, it will have an empty set of permissions
@@ -42,12 +42,13 @@ class Acl(object):
     def add_permission(self, resource, permission):
         """ Define permission on a resource
 
-            :param resources: Existing resource to define the permission on.
+            :param resources: Resource to define the permission on.
             :param permission: Permission to define.
             :rtype: Acl
 
             Existing permissions are not overwritten nor duplicated
         """
+        self.add_resource(resource)
         self._structure[resource].add(permission)
         return self
 
@@ -55,8 +56,15 @@ class Acl(object):
 
     #region Delete
 
+    def clear(self):
+        """ Clear the Acl """
+        self._roles.clear()
+        self._structure.clear()
+        self._grants.clear()
+        return self
+
     def del_role(self, role):
-        """ Remove role and their grants.
+        """ Remove a role and its grants.
 
             :param roles: Role to remove
             :rtype: Acl
@@ -65,7 +73,7 @@ class Acl(object):
         return self
 
     def del_resource(self, resource):
-        """ Remove resource along with its grants and permissions
+        """ Remove a resource along with its grants and permissions
 
             :param resource: Resource to remove
             :rtype: Acl
@@ -74,22 +82,42 @@ class Acl(object):
             del self._structure[resource]
         return self
 
+    def del_permission(self, resource, permission):
+        """ Remove a permission from the resource, along with its grants
+
+            :param resource: The resource to remove the permission from
+            :param permission: Permission to remove
+            :rtype: Acl
+        """
+        if resource in self._structure:
+            self._structure[resource].discard(permission)
+        return self
+
     #endregion
 
     #region List
 
     def list_roles(self):
-        """ Get the list of defined roles.
+        """ Get the list of roles.
 
             :rtype: list
         """
         return list(self._roles)
 
     def list_resources(self):
-        """ Get the list of defines resources
+        """ Get the list of resources
 
             :rtype: list
         """
         return self._structure.keys()
+
+    def list_permissions(self, resource):
+        """ Get the list of permissions on a resource
+
+            :rtype: list
+        """
+        if resource not in self._structure:
+            return []
+        return list(self._structure[resource])
 
     #endregion
--- tests/acl-test.py
+++ tests/acl-test.py
@@ -8,7 +8,7 @@ class TestAclStructure(unittest.TestCase):
 
         acl.add_role('root')
         acl.add_role('superadmin')
-        acl.add_role('superadmin')
+        acl.add_role('superadmin') # does not replace or fail
         acl.add_role('user')
         acl.add_role('poweruser')
         acl.add_role('n00b')
@@ -19,7 +19,7 @@ class TestAclStructure(unittest.TestCase):
         )
 
         acl.del_role('poweruser')
-        acl.del_role('poweruser')
+        acl.del_role('poweruser') # does not fail
         acl.del_role('n00b')
 
         self.assertListEqual(
@@ -33,7 +33,7 @@ class TestAclStructure(unittest.TestCase):
 
         acl.add_resource('user')
         acl.add_resource('page')
-        acl.add_resource('page')
+        acl.add_resource('page') # does not replace or fail
         acl.add_resource('news')
         acl.add_resource('blog')
 
@@ -43,10 +43,76 @@ class TestAclStructure(unittest.TestCase):
         )
 
         acl.del_resource('news')
-        acl.del_resource('news')
+        acl.del_resource('news') # does not fail
         acl.del_resource('blog')
 
         self.assertListEqual(
             sorted(acl.list_resources()),
             sorted(['user', 'page'])
+        )
+
+    def test_permissions(self):
+        """ add_permission(), list_permissions(), del_permission() """
+        acl = miracle.Acl()
+
+        acl.add_permission('user', 'create') # silently creates a resource
+        acl.add_permission('user', 'create') # does not replace or fail
+        acl.add_permission('user', 'read')
+        acl.add_permission('user', 'write')
+        acl.add_permission('post', 'read')
+        acl.add_permission('post', 'create')
+        acl.add_permission('log', 'delete')
+
+        self.assertListEqual(
+            sorted(acl.list_resources()),
+            sorted(['user', 'post', 'log'])
+        )
+
+        self.assertListEqual(
+            sorted(acl.list_permissions('404')), # empty ok
+            sorted([])
+        )
+
+        self.assertEqual(
+            sorted(acl.list_permissions('user')),
+            sorted(['create','read','write'])
+        )
+
+        self.assertEqual(
+            sorted(acl.list_permissions('post')),
+            sorted(['read', 'create'])
+        )
+
+        self.assertEqual(
+            sorted(acl.list_permissions('log')),
+            sorted(['delete'])
+        )
+
+        acl.del_permission('user', 'write')
+        acl.del_permission('post', 'create')
+        acl.del_permission('post', 'create') # does not fail
+
+        self.assertListEqual(
+            sorted(acl.list_resources()),
+            sorted(['user', 'post', 'log'])
+        )
+
+        self.assertListEqual(
+            sorted(acl.list_permissions('404')), # empty ok
+            sorted([])
+        )
+
+        self.assertEqual(
+            sorted(acl.list_permissions('user')),
+            sorted(['create', 'read'])
+        )
+
+        self.assertEqual(
+            sorted(acl.list_permissions('post')),
+            sorted(['read'])
+        )
+
+        self.assertEqual(
+            sorted(acl.list_permissions('log')),
+            sorted(['delete'])
         )
